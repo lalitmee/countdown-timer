3e47a8024917271986bd676fbb11e57afb1c9300
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2020-10-25 23:54:43 +0530

    Updates

---
+++ precache-manifest.fcdc0b2b78e5612c05d3b09252f2f914.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e0891e1aa2412a50f68d4e604d0aab",
+    "url": "/countdown-timer/index.html"
+  },
+  {
+    "revision": "c6e34f5109012b5bd5b1",
+    "url": "/countdown-timer/static/js/2.7c86a2cf.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/countdown-timer/static/js/2.7c86a2cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb4cb3f6f8223458d02",
+    "url": "/countdown-timer/static/js/main.94855039.chunk.js"
+  },
+  {
+    "revision": "e5f6f6931ccaad3b619f",
+    "url": "/countdown-timer/static/js/runtime-main.aefea532.js"
+  }
+]);
