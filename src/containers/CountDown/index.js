a5058d5742626eb568a8dd6d944eb2c19756f605
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2020-10-26 04:31:00 +0530

    Fix time for seconds

--- src/containers/CountDown/index.js
+++ src/containers/CountDown/index.js
@@ -47,7 +47,7 @@ function CountDown() {
   useEffect(() => {
     let interval;
     if (isRunning) {
-      const newCountDownTime = countDownTime - 21;
+      const newCountDownTime = countDownTime - 10;
       interval = setInterval(() => {
         setCountDownTime(newCountDownTime);
       }, 10);
